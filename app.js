6a9c0cf99ac1d11cbc1bf488a1a9956beb63788a
Dadadon <davianberoni@yahoo.com>
2022-07-27 06:18:17 -0400

    update app.js

--- app.js
+++ app.js
@@ -89,3 +89,16 @@ featuredCards.forEach((el, i) => {
   grab("feat-cards").appendChild(pro);
 });
 
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+  document.body.style.overflow = "hidden";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+  document.body.style.overflow = "visible";
+}
+
+openNav();
+closeNav();
+
